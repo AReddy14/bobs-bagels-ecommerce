196b94b156cffd8ed5e73316fa86bc7f71c1165d
areddy14 <areddy1441@gmail.com>
2014-11-12 12:21:06 -0500

    added some basic functionality to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+	def index
+    @users = User.all
+    render json: @users
+  end
+# Not sure if I want index to be around or rather I don't think we want this to ever be called
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :email, :password, :password_confirmation)
+  end
 end
