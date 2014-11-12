837f9aeaad8f2746d82441550b976e5090f31931
areddy14 <areddy1441@gmail.com>
2014-11-12 12:44:08 -0500

    I've added a helper_method current_user to the ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+	include ActionController::ImplicitRender
+	# Not sure I want the above in here, more research required.
+  helper_method :current_user
+
+	private
+
+	def current_user
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 end
